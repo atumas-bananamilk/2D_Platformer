1603601526b9e98aae5b0d28cc51d3f68ba8c058
Aivaras T <aiwaras1111@gmail.com>
2018-07-23 13:55:56 +0200

    asdasd

--- Assets/Scripts/photonConnect.cs
+++ Assets/Scripts/photonConnect.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class photonConnect : MonoBehaviour {
     public string versionName = "0.1";
-    public GameObject section_view_1, section_view_2, section_view_3;
+    //public GameObject section_view_1, section_view_2, section_view_3;
 
     //public void connectToPhoton()
     //{
@@ -19,27 +19,27 @@ public class photonConnect : MonoBehaviour {
     }
 
     private void OnConnectedToMaster(){
-        section_view_2.SetActive(true);
-        section_view_3.SetActive(false);
+        //section_view_2.SetActive(true);
+        //section_view_3.SetActive(false);
 
         PhotonNetwork.JoinLobby(TypedLobby.Default);
         Debug.Log("CONNECTED TO MASTER");
     }
 
     private void OnJoinedLobby(){
-        section_view_1.SetActive(false);
-        section_view_2.SetActive(true);
+        //section_view_1.SetActive(false);
+        //section_view_2.SetActive(true);
         Debug.Log("JOINED LOBBY");
     }
 
     private void OnDisconnectedFromPhoton(){
-        if (section_view_1.GetActive()){
-            section_view_1.SetActive(false);
-        }
-        if (section_view_2.GetActive()){
-            section_view_2.SetActive(false);
-        }
-        section_view_3.SetActive(true);
+        //if (section_view_1.GetActive()){
+        //    section_view_1.SetActive(false);
+        //}
+        //if (section_view_2.GetActive()){
+        //    section_view_2.SetActive(false);
+        //}
+        //section_view_3.SetActive(true);
 
         Debug.Log("DISCONNECTED");
 
