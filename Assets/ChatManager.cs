7f0d42ee04b8312fa77cbf704c0a8910a395dd45
Aivaras T <aiwaras1111@gmail.com>
2018-08-14 19:40:02 +0200

    asdasd

--- Assets/ChatManager.cs
+++ Assets/ChatManager.cs
@@ -8,7 +8,7 @@ public class ChatManager : MonoBehaviour {
     public InputField chat_input;
     public Button chat_button;
     public Button chat_send_button;
-    public Text player_chat_text;
+    public Text local_player_chat_text;
 
 	// Use this for initialization
     public void OpenChatWindow(){
@@ -16,6 +16,9 @@ public class ChatManager : MonoBehaviour {
     }
 
     public void SendChatMessage(){
-        player_chat_text.text = chat_input.text;
+        if (gameObject.GetComponent<playerMove>().view.isMine)
+        {
+            local_player_chat_text.text = chat_input.text;
+        }
     }
 }
